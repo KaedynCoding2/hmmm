f208490292b3d4a07525a82a53e6311a6c99a6d1
KaedynCoding2 <161186555+KaedynCoding2@users.noreply.github.com>
2024-02-24 21:09:23 -0800

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,44 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "Bulbasaur":return img`
+. . . . . . . . . . c c c c . 
+. . . . . c 7 7 c c c c c c . 
+6 c c . c 7 b b 7 7 7 7 7 c . 
+b b b b 6 7 b b 7 b b 7 7 c c 
+b d b b 6 b b b b 7 7 7 c c c 
+b d b 6 6 b d d b 7 7 c c c c 
+b b b b b b c b b c c c c c c 
+b b 6 6 b d b e 6 c c c c c c 
+b d d d d d b e b 6 c c c c c 
+b b b b b d b b 6 6 6 c c c c 
+c c b b b b c 6 b b b 6 6 c c 
+c 6 b b b b 6 6 b b b 6 6 6 c 
+c 6 6 6 c 6 6 b b b 6 6 6 6 c 
+c c 6 b 6 c c c b b 6 6 6 6 c 
+. c b b c c c b b c c b b c 6 
+. c c c . . c b c c c c c c . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
